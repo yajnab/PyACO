e06d973b8f40dde0fff6451e1cf185d466051269
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2017-04-06 00:30:37 +0530

    PyACO.py : Import Numpy Package

--- PyACO.py
+++ PyACO.py
@@ -1,5 +1,5 @@
+import numpy as np
 class PyACO:
 	n_val = input("Enter the Number of Variables");
 	maxiter = input("Enter the maximum Iterations");
 	maxroute = input("Enter the maximum amount of routes");
-	
