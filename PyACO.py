cbb5f4afa7369f292ab0061043610726e7c0fd51
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2017-04-06 01:04:57 +0530

    PyACO.py : Constraints Bound acceptance

--- PyACO.py
+++ PyACO.py
@@ -1,5 +1,10 @@
 import numpy as np
 class PyACO:
-	n_val = input("Enter the Number of Variables");
+	n_val = int(input("Enter the Number of Variables"));
 	maxiter = input("Enter the maximum Iterations");
 	maxroute = input("Enter the maximum amount of routes");
+	bounds = np.ones((n_val,2), dtype=float)
+	print("Enter the Lower and Upper bounds of each variable \n")
+	for i in range(n_val):
+		for j in range(2):
+			bounds[1,j]=input("Enter");
