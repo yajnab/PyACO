cf324bcbfce2f4754d003fc896477cb8e82cb920
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2017-04-06 01:06:51 +0530

    PyACO : Update the datatype for bounds array

--- PyACO.py
+++ PyACO.py
@@ -3,7 +3,7 @@ class PyACO:
 	n_val = int(input("Enter the Number of Variables"));
 	maxiter = input("Enter the maximum Iterations");
 	maxroute = input("Enter the maximum amount of routes");
-	bounds = np.ones((n_val,2), dtype=float)
+	bounds = np.ones((n_val,2), dtype=np.float64)
 	print("Enter the Lower and Upper bounds of each variable \n")
 	for i in range(n_val):
 		for j in range(2):
