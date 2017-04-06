508be153875eed302e35695491654ef8dc341544
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2017-04-07 02:19:40 +0530

    prob.py : Script for Problem to be optimized with constraints
    *val variable will have the problem equation
    *cnstr[array] will be the constraint equation

---
+++ prob.py
@@ -0,0 +1,20 @@
+import numpy as np #Import Numpy
+
+def func(n_val, a):
+	x = np.ones(n_val, dtype=float64)
+
+
+	'''Change the part from here'''
+
+	val =  #Calculate the Value
+
+	#Constraints goes here
+	n_cnstr = 3 #Number of Constraints
+	cnstr = np.ones(n_cnstr, dtype=np.float64) #Declare the constraints array
+	cnstr[0] = #1st Constraints Goes Here
+	cnstr[1] = #2nd Constraint Goes Here
+	cnstr[2] = #3rd Constraint Goes Here
+
+	
+
+	
