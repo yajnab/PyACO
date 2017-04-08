eeff4ea3fbcac6cb111d0efdbfe40408caf09e69
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2017-04-09 02:22:55 +0530

    prob.py : Constraints are in the form of g(x)>0

--- prob.py
+++ prob.py
@@ -15,7 +15,7 @@ def func(n_val, a):
 	val = 2*x[0] + 4*x[1]/x[0] + x[0]*x[1]*x[2]  #Calculate the Value
 
 	#Constraints goes here
-	n_cnstr = 3 #Number of Constraints
+	n_cnstr = 3 #Number of Constraints in the form of g(x) => 0
 	cnstr = np.ones(n_cnstr, dtype=np.float64) #Declare the constraints array
 	cnstr[0] = x[0]*x[1] - 1 #1st Constraints Goes Here
 	cnstr[1] = 2*x[0] + x[1];#2nd Constraint Goes Here
@@ -24,7 +24,7 @@ def func(n_val, a):
 	'''Do Not Change any Code below'''
 	validate = int(1)
 	for i in range(n_cnstr):
-		if (cnstr[i] > 0):
+		if (cnstr[i] < 0):
 			validate = 0
 
 	result = [val, validate]
