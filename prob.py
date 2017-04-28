5827fab3eac1dbd9a2aae1440cc8aabacef172f4
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2017-04-28 23:19:31 +0530

    prob.py: Update Problem as per the function requirement

--- prob.py
+++ prob.py
@@ -5,28 +5,19 @@
  * Techno India College of Technology
 '''
 import numpy as np #Import Numpy
+from PyACO import pyco
+class prob:
 
-def func(a):
-	x = a
-
-
-	'''Change the part from here'''
-
-	val = 2*x[0] + 4*x[1]/x[0] + x[0]*x[1]*x[2]  #Calculate the Value
-
+	def func(x):
+		val = 2*x[0] + 4*x[1]/x[0] + x[0]*x[1]*x[2]  #Calculate the Value
+		return(val)
 	#Constraints goes here
-	n_cnstr = 3 #Number of Constraints in the form of g(x) => 0
-	cnstr = np.ones(n_cnstr, dtype=np.float64) #Declare the constraints array
-	cnstr[0] = x[0]*x[1] - 1 #1st Constraints Goes Here
-	cnstr[1] = 2*x[0] + x[1];#2nd Constraint Goes Here
-	cnstr[2] = x[2]-85#3rd Constraint Goes Here
-	
-	'''Do Not Change any Code below'''
-	validate = int(1)
-	for i in range(n_cnstr):
-		if (cnstr[i] < 0):
-			validate = 0
+	#n_cnstr = 3
+	#cnstr = np.ones(n_cnstr, dtype=np.float64) #Declare the constraints array
+	#Number of Constraints in the form of g(x) => 0
+	def cons(cnstr, x):
+		cnstr[0] = x[0]*x[1] - 1 #1st Constraints Goes Here
+		cnstr[1] = 2*x[0] + x[1];#2nd Constraint Goes Here
+		cnstr[2] = x[2]-85#3rd Constraint Goes Here
 
-	result = [validate, val, a]
-	return result
-	
+	PyACO(func, cons, n_val, maxiter, maxroute, phe)
