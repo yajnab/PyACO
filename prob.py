a6612e7f056d0475b1e6bc30884e791ce1fcb6c8
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2017-04-28 23:45:52 +0530

    prob.py : Code as per requirement of PyACO.py
    Signed-off-by: Yajnavalkya Bandyopadhyay <yajnab@gmail.com>

--- prob.py
+++ prob.py
@@ -5,19 +5,25 @@
  * Techno India College of Technology
 '''
 import numpy as np #Import Numpy
-from PyACO import pyco
+from PyACO import *
 class prob:
 
 	def func(x):
 		val = 2*x[0] + 4*x[1]/x[0] + x[0]*x[1]*x[2]  #Calculate the Value
 		return(val)
 	#Constraints goes here
-	#n_cnstr = 3
-	#cnstr = np.ones(n_cnstr, dtype=np.float64) #Declare the constraints array
+
 	#Number of Constraints in the form of g(x) => 0
-	def cons(cnstr, x):
+	def cons(x):
+		n_cnstr = 3
+		cnstr = np.ones(n_cnstr, dtype=np.float64) #Declare the constraints array
 		cnstr[0] = x[0]*x[1] - 1 #1st Constraints Goes Here
 		cnstr[1] = 2*x[0] + x[1];#2nd Constraint Goes Here
 		cnstr[2] = x[2]-85#3rd Constraint Goes Here
+		return(cnstr)
 
+	n_val=3
+	maxiter=3
+	maxroute=10
+	phe=0.2
 	PyACO(func, cons, n_val, maxiter, maxroute, phe)
