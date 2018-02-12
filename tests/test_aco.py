5525dd5cff4fe8ff4d4c9f3e6d1052baf633076c
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2018-02-12 23:58:07 +0530

    test_pyaco.py : Fix 2D array intake for bounds

--- tests/test_aco.py
+++ tests/test_aco.py
@@ -26,7 +26,7 @@ class prob:
 	maxiter=3
 	maxroute=10
 	phe=0.2
-	bounds = np.array([60,120],[60,120],[60,120])
+	bounds = np.array([[60,120],[60,120],[60,120]])
 	m = PyACO(func, cons, n_val, bounds, maxiter, maxroute, phe)
 	print("Minimum Value is", m[0])
 	print("Variables are", m[1])
